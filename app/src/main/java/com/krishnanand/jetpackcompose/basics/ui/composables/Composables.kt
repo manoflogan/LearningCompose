ea3971a9926dff04fcf01f46f4e1da87f1528368
manoflogan <manoflogan@gmail.com>
2022-06-05 21:38:56 -0700

    Creating mutable state variabe to handle expanded state.

--- app/src/main/java/com/krishnanand/jetpackcompose/basics/ui/composables/Composables.kt
+++ app/src/main/java/com/krishnanand/jetpackcompose/basics/ui/composables/Composables.kt
@@ -1,16 +1,24 @@
 package com.krishnanand.jetpackcompose.basics.ui.composables
 
-import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.krishnanand.jetpackcompose.basics.ui.theme.JetpackComposeBasicsTheme
@@ -23,13 +31,22 @@ fun Greeting(name: String) {
             .padding(horizontal = 8.dp, vertical = 4.dp),
         color = MaterialTheme.colors.primary
     ) {
-         Column(
-            modifier = Modifier
-                .padding(24.dp)
-                .fillMaxWidth()
-         ) {
-             Text(text = "Hello")
-             Text(text = name)
+        Row(modifier= Modifier
+            .padding(24.dp)
+            .fillMaxWidth()) {
+            var isExpanded by remember {
+                mutableStateOf(false)
+            }
+           Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = {
+                    isExpanded = !isExpanded
+                }
+            ) {
+                    Text(text = if (isExpanded) "Show Less" else "Show More")
+            }
         }
     }
 }
