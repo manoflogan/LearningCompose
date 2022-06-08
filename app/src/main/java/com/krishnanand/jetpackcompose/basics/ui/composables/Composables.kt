b601f7ef4bbe59afa85b4240568f73546db2edba
manoflogan <manoflogan@gmail.com>
2022-06-08 00:35:21 -0700

    Retaining state by using rememberSaveable for onbooarding state even during configuration chang even during configuration change

--- app/src/main/java/com/krishnanand/jetpackcompose/basics/ui/composables/Composables.kt
+++ app/src/main/java/com/krishnanand/jetpackcompose/basics/ui/composables/Composables.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -79,7 +80,7 @@ fun DefaultPreview() {
 fun MyApp(names: List<String> = List(100) {
     it.toString()
 }) {
-    var onboarding by remember { mutableStateOf(true) }
+    var onboarding by rememberSaveable { mutableStateOf(true) }
     if (onboarding) {
         OnboardingScreen {
             onboarding = false
